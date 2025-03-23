b0c3c4cd1d317d087463483155b7d500ac23d560
Samhithaa07 <j.samhitha.k@gmail.com>
2025-03-23 14:23:55 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,31 +1,31 @@
 document.getElementById("check").addEventListener("click", async () => {
-    const url = document.getElementById("url").value;
-  
-    if (!url) {
-      document.getElementById("result").innerText = "Please enter a URL.";
-      return;
-    }
-  
-    try {
-      const response = await fetch("http://localhost:5001/check-url", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ url }),
-      });
-  
-      const data = await response.json();
-  
-      if (data.isMalicious) {
-        document.getElementById("result").innerText =
-          "Warning: This URL is potentially malicious!";
-      } else {
-        document.getElementById("result").innerText = "This URL is safe.";
-      }
-    } catch (error) {
-      console.error("Error checking URL:", error);
+  const url = document.getElementById("url").value;
+
+  if (!url) {
+    document.getElementById("result").innerText = "Please enter a URL.";
+    return;
+  }
+
+  try {
+    const response = await fetch("http://localhost:5001/check-url", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url }),
+    });
+
+    const data = await response.json();
+
+    if (data.isMalicious) {
       document.getElementById("result").innerText =
-        "An error occurred while checking the URL.";
+        "Warning: This URL is potentially malicious!";
+    } else {
+      document.getElementById("result").innerText = "This URL is safe.";
     }
-  });
+  } catch (error) {
+    console.error("Error checking URL:", error);
+    document.getElementById("result").innerText =
+      "An error occurred while checking the URL.";
+  }
+});
